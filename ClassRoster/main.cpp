4f202dabbd9fd4de2761aea58673bd5ac1c55e27
Taylor <ktaylorbailey@gmail.com>
2024-03-28 16:14:29 -0400

    Refactors Student class variables to initialize at same time as definition and adds printInvalidEmails and printByDegreeProgram Roster methods

--- ClassRoster/main.cpp
+++ ClassRoster/main.cpp
@@ -19,8 +19,11 @@ int main() {
     roster.parse("A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY");
     roster.parse("A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK");
     roster.parse("A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE");
-    roster.remove("A2");
-    roster.remove("A3");
+    roster.parse("A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY");
+    roster.parse("A5,Kevin,Bailey,kbai350@wgu.edu,30,15,20,16,SOFTWARE");
+    roster.printAll();
+    roster.printInvalidEmails();
+    roster.printByDegreeProgram(SECURITY);
 
     return 0;
 }
--- ClassRoster/roster.cpp
+++ ClassRoster/roster.cpp
@@ -2,59 +2,48 @@
 #include <iostream>
 
 void Roster::parse(std::string dataRow) {
-	// Variables to store parsed data to pass to add method
-	std::string studentID;
-	std::string firstName;
-	std::string lastName;
-	std::string email;
-	int age;
-	int daysInCourse1;
-	int daysInCourse2;
-	int daysInCourse3;
-	DegreeProgram degree;
-
 	// Stores the passed data string to be parsed
 	std::string dataText = dataRow;
-	int commaPosition;
 
-	commaPosition = dataText.find(',');
-	studentID = dataText.substr(0, commaPosition - 0);
+	int commaPosition = dataText.find(',');
+	std::string studentID = dataText.substr(0, commaPosition - 0);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
-	firstName = dataText.substr(0, commaPosition - 0);
+	std::string firstName = dataText.substr(0, commaPosition - 0);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
-	lastName = dataText.substr(0, commaPosition - 0);
+	std::string lastName = dataText.substr(0, commaPosition - 0);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
-	email = dataText.substr(0, commaPosition - 0);
+	std::string email = dataText.substr(0, commaPosition - 0);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
 	std::string ageString = dataText.substr(0, commaPosition - 0);
-	age = std::stoi(ageString);
+	int age = std::stoi(ageString);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
 	std::string days1String = dataText.substr(0, commaPosition - 0);
-	daysInCourse1 = std::stoi(days1String);
+	int daysInCourse1 = std::stoi(days1String);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
 	std::string days2String = dataText.substr(0, commaPosition - 0);
-	daysInCourse2 = std::stoi(days2String);
+	int daysInCourse2 = std::stoi(days2String);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
 	std::string days3String = dataText.substr(0, commaPosition - 0);
-	daysInCourse3 = std::stoi(days3String);
+	int daysInCourse3 = std::stoi(days3String);
 	dataText.erase(0, commaPosition + 1);
 
 	commaPosition = dataText.find(',');
 	std::string degreeString = dataText.substr(0, commaPosition - 0);
+	DegreeProgram degree;
 	if (degreeString == "SECURITY") {
 		degree = SECURITY;
 	}
@@ -83,37 +72,26 @@ void Roster::add(
 	std::cout << "Last student Index after adding: " << lastStudentIndex << '\n';
 }
 
-// breaks program if called twice
+// REWORK
 void Roster::remove(std::string studentID) {
 	bool found = false;
-	std::cout << "Before for loop" << '\n';
 
-	for (int i = 0; i <= Roster::lastStudentIndex; i++) {
-		std::cout << "In for loop" << '\n';
+	for (int i = 0; i < numStudents; i++) {
 		if (classRosterArray[i]->getStudentID() == studentID) {
-			std::cout << "In for loop if statement" << '\n';
 			found = true;
 
-			// If the object found is not the last item in the classRosterArray it will swap places with the next student object in the array
 			if (i < numStudents - 1) {
-				std::cout << "Before Student* temp = classRosterArray[i];" << '\n';
 				Student* temp = classRosterArray[i];
-				std::cout << "Before classRosterArray[i] = classRosterArray[numStudents - 1];" << '\n';
 				classRosterArray[i] = classRosterArray[numStudents - 1];
-				std::cout << "Before classRosterArray[numStudents - 1] = temp;" << '\n';
 				classRosterArray[numStudents - 1] = temp;
-				std::cout << "End of nested if" << '\n';
 			}
 
 			// This decrements the number of visible student objects in the classRosterArray to hide the removed student object
-			std::cout << "Before Roster::lastStudentIndex--;" << '\n';
-			Roster::lastStudentIndex--;
-			std::cout << "Last student index after removing: " << lastStudentIndex << '\n';
+			lastStudentIndex--;
+			std::cout << "Last student Index after removing: " << lastStudentIndex << '\n';
 			break;
 		}
 	}
-
-	std::cout << "Last student index after removing and leaving for loop: " << lastStudentIndex << '\n';
 
 	if (found) {
 		std::cout << "The student was removed." << '\n';
@@ -126,7 +104,7 @@ void Roster::remove(std::string studentID) {
 void Roster::printAll() {
 	Student::printHeader();
 
-	for (int i = 0; i <= Roster::lastStudentIndex; i++) {
+	for (int i = 0; i <= lastStudentIndex; i++) {
 		classRosterArray[i]->print();
 	}
 }
@@ -134,7 +112,7 @@ void Roster::printAll() {
 void Roster::printAverageDaysInCourse(std::string studentID) {
 	bool found = false;
 	
-	for (int i = 0; i <= Roster::lastStudentIndex; i++) {
+	for (int i = 0; i <= lastStudentIndex; i++) {
 		if (classRosterArray[i]->getStudentID() == studentID) {
 			found = true;
 
@@ -143,11 +121,32 @@ void Roster::printAverageDaysInCourse(std::string studentID) {
 			int class3 = std::stoi(classRosterArray[i]->getDaysToComplete(2));
 
 			int average = (class1 + class2 + class3) / 3;
-			std::cout << average << '\n';
+			std::cout << "Student ID: " << studentID << ", Average days in course: " << average << '\n';
 		}
 	}
 
 	if (!found) {
 		std::cout << "The student with the ID: " << studentID << " Was not found." << '\n';
+	}
+}
+
+void Roster::printInvalidEmails() {
+	for (int i = 0; i < numStudents; i++) {
+		std::string studentEmail = classRosterArray[i]->getEmail();
+
+		if ((studentEmail.find('@') == std::string::npos) || (studentEmail.find('.') == std::string::npos) || (studentEmail.find(' ') != std::string::npos)) {
+			std::cout << studentEmail << " - is invalid." << '\n';
+		}
+	}
+}
+
+void Roster::printByDegreeProgram(DegreeProgram degreeProgram) {
+	std::string degreeString = degreeProgramString[degreeProgram];
+	Student::printHeader();
+	
+	for (int i = 0; i < numStudents; i++) {
+		if (classRosterArray[i]->getDegree() == degreeString) {
+			classRosterArray[i]->print();
+		}
 	}
 }
